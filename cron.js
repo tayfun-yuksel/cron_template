53be03aa6dbb02d0b5a2bfe0427c560c779d6b4f
Tayfun Yüksel <tyueksel@sw.buhl-data.com>
2023-02-16 22:56:57 +0100

    create/delete/edit

--- cron.js
+++ cron.js
@@ -1,250 +1,151 @@
-// load.js
-const data = [
-    {
-      Id: 1,
-      Name: "Task 1",
-      Status: "Running",
-      Schedule: "Daily",
-      "Success-Count": 5,
-      "Error-Count": 0,
-      Enabled: true,
-      Retries: 3,
-      Next: "2023-02-14T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
-    {
-      Id: 2,
-      Name: "Task 2",
-      Status: "Success",
-      Schedule: "Weekly",
-      "Success-Count": 10,
-      "Error-Count": 1,
-      Enabled: true,
-      Retries: 2,
-      Next: "2023-02-21T08:00:00"
-    },
+// table elements 
+const table = document.querySelector("table tbody");
+const createButton = document.querySelector("#create");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Add leading zero to single-digit months
+  const day = String(date.getDate()).padStart(2, "0"); // Add leading zero to single-digit days
+
+  return `${year}-${month}-${day}`;
+}
+
+function generateMockData(){
+  const data = [
+    
     // ... add more items here
   ];
-  
+  let statusList = ["Waiting", "Failed", "Enabled", "Disabled"]
+  let scheduleList = ["Daily", "Hourly", "Weekly", "Monthly"]
+  for (let index = 0; index < 40; index++) {
+    // Generate a random uppercase letter from A to Z
+    let randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      data.push(
+        {
+          Id: index,
+          Name: "Task " + randomLetter,
+          Status: statusList[Math.floor(Math.random()*4)],
+          Schedule: scheduleList[Math.floor(Math.random()*4)],
+          "Success-Count": 5,
+          "Error-Count": 0,
+          LastSuccess: formatDate(new Date()),
+          LastError: formatDate(new Date()),
+          Enabled: true,
+          Retries: 3,
+          Next: "2023-02-14"
+        },
+      );
+  }
   window.data = data;
+}
 
 
-  const table = document.querySelector("table tbody");
-  const createButton = document.querySelector("#create");
-  const clearButton = document.querySelector("#clear");
-  const deleteButton = document.querySelector("#delete");
 
+//event for check boxes when one of the rows toggled
+function addEventListenerCheckbox(){
   table.addEventListener("change", (event) => {
-  const checkbox = event.target;
-  if (checkbox.tagName === "INPUT" && checkbox.type === "checkbox") {
-      let checkedCheckboxes = table.querySelectorAll("input[type=checkbox]:checked");
-      if (checkedCheckboxes.length > 0) {
-      clearButton.style.display = "inline-block";
-      deleteButton.style.display = "inline-block";
-      } else {
-      clearButton.style.display = "none";
-      deleteButton.style.display = "none";
-      }
-  }
+    const checkbox = event.target;
+    if (checkbox.tagName === "INPUT" && checkbox.type === "checkbox") {
+        let checkedCheckboxes = table.querySelectorAll("input[type=checkbox]:checked");
+        if (checkedCheckboxes.length > 0) {
+          clearButton.style.display = "inline-block";
+          deleteButton.style.display = "inline-block";
+        } else {
+          clearButton.style.display = "none";
+          deleteButton.style.display = "none";
+        }
+    }
   });
+}
+  
 
+function createCheckBox(){
+  const checkboxTd = document.createElement("td");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkboxTd.appendChild(checkbox);
+  addEventListenerCheckbox();
+
+  return checkboxTd;
+}
+
+
+function createEditButton(item){
+  const editButton = document.createElement("button");
+  editButton.className = "btn btn-primary";
+  editButton.textContent = "Edit";
+  editButton.addEventListener("click", () => {
+          window.location.href = "edit.html?id=" + item.Id;
+          sessionStorage.setItem("editCronItem", JSON.stringify(item));
+      });
+  const editButtonTd = document.createElement("td");
+  editButtonTd.appendChild(editButton);
+  return editButtonTd;
+}
+
+
+function addEventListenerToCreateButton(){
+  createButton.addEventListener("click", () => {
+    window.location.href = "edit.html";
+    sessionStorage.setItem("editCronItem", "");
+  });
+}
+
+
+function addEventListenerToDeleteButton() {
+  deleteButton.addEventListener("click", () => {
+    const checkedCheckboxes = table.querySelectorAll("input[type=checkbox]:checked");
+    checkedCheckboxes.forEach((checkbox) => {
+      const row = checkbox.parentNode.parentNode;
+      table.removeChild(row);
+    });
+    clearButton.style.display = "none";
+    deleteButton.style.display = "none";
+  });
+}
+
+
+
+function fillRow(tr, cronData){
+  for (const key in cronData) {
+    const td = document.createElement("td");
+    td.textContent = cronData[key];
+    td.setAttribute("id", key)
+    tr.appendChild(td);
+  }
+
+}
+
+function constructTable(){
+  //note: window-data is used temporary, 
+  //in c# project this data will come from api
   for (const item of window.data) {
-      const tr = document.createElement("tr");
-
-      const checkboxTd = document.createElement("td");
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-
-      checkboxTd.appendChild(checkbox);
-      tr.appendChild(checkboxTd);
-      
-      for (const key in item) {
-          const td = document.createElement("td");
-          td.textContent = item[key];
-          tr.appendChild(td);
-      }
-
-      const editButton = document.createElement("button");
-      editButton.className = "btn btn-primary";
-      editButton.textContent = "Edit";
-      editButton.addEventListener("click", () => {
-              window.location.href = "edit.html?id=" + item.id;
-          });
-      const editButtonTd = document.createElement("td");
-      editButtonTd.appendChild(editButton);
-
-
-      // const deleteButton = document.createElement("button");
-      // deleteButton.className = "btn btn-danger";
-      // deleteButton.textContent = "Delete";
-      // const deleteButtonTd = document.createElement("td");
-      // deleteButtonTd.appendChild(deleteButton);
-
-
-      tr.appendChild(editButtonTd);
-      // tr.appendChild(deleteButtonTd);
-
-      table.appendChild(tr);
+    const tr = document.createElement("tr");
+    const checkboxTd = createCheckBox();
+    tr.appendChild(checkboxTd);
+    
+    fillRow(tr, item);
+    // edit button for each row
+    const  editButtonTd =  createEditButton(item);
+    tr.appendChild(editButtonTd);
+    table.appendChild(tr);
   }
+}
+ 
 
-  function clearCheckBoxes(){
-      $('input[type="checkbox"]:checked').prop('checked',false);
-      clearButton.style.display = "none";
-      deleteButton.style.display = "none";
-  }
+function clearCheckBoxes(){
+    $('input[type="checkbox"]:checked').prop('checked',false);
+    clearButton.style.display = "none";
+    deleteButton.style.display = "none";
+}
 
+
+
+generateMockData();
+constructTable();
+addEventListenerToCreateButton();
+addEventListenerToDeleteButton();
 //   export default data;
   
--- edit.js
+++ edit.js
@@ -1,33 +1,48 @@
+const id = document.getElementById("id").value;
+const cronName = document.getElementById("name").value;
+const schedule = document.getElementById("schedule").value;
+const enabled = document.getElementById("enabled").value;
+const next = document.getElementById("next").value;
+
+
 function back(){
-    window.location.href="cron.html";
+  window.location.href="cron.html";
 }
 
-function getFormData(){
-    
+
+function setFormData(){
+  var sessionData = sessionStorage.getItem("editCronItem");
+  if(sessionData === "") return;
+
+  const {Id, Name, Enabled, Schedule, Next} = JSON.parse(sessionData);
+  console.log("id: " + Id);
+  // Get form inputs and update their values with item data
+  const nameInput = document.getElementById("name");
+  nameInput.value = Name;
+  const enabledInput = document.getElementById("enabled");
+  enabledInput.checked = Enabled;
+  const scheduleInput = document.getElementById("schedule");
+  scheduleInput.value = Schedule;
+  const nextInput = document.getElementById("next");
+  nextInput.value = Next;7
+  const idInput = document.getElementById("id");
+  idInput.value = Id;
+  idInput.disabled = true;
 }
+
+
 
 function saveData() {
-  const id = document.getElementById("id").value;
-  const name = document.getElementById("name").value;
-  const status = document.getElementById("status").value;
-  const schedule = document.getElementById("schedule").value;
-  const successCount = document.getElementById("successCount").value;
-  const errorCount = document.getElementById("errorCount").value;
-  const enabled = document.getElementById("enabled").value;
-  const retries = document.getElementById("retries").value;
-  const next = document.getElementById("next").value;
+  console.log("calling save ....");
+  const formData = {
+    id,
+    cronName,
+    schedule,
+    enabled,
+    next
+  }
 
-  const formData = new FormData();
-  formData.append("id", id);
-  formData.append("name", name);
-  formData.append("status", status);
-  formData.append("schedule", schedule);
-  formData.append("successCount", successCount);
-  formData.append("errorCount", errorCount);
-  formData.append("enabled", enabled);
-  formData.append("retries", retries);
-  formData.append("next", next);
-
+  //TODO: use real api here
   fetch("https://example.com/api/update-data", {
     method: "POST",
     body: formData
@@ -35,7 +50,6 @@ function saveData() {
     .then(response => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
-      alert("error while savinf");
       }
       return response.json();
     })
@@ -45,7 +59,25 @@ function saveData() {
     })
     .catch(error => {
       console.error("There was a problem with the fetch operation:", error);
-      alert("error while savinf");
+      //alert("error while savinf");
+      back();
       // handle the error case here
     });
 }
+
+function handleFormSubmit(){
+  const form = document.getElementById('cron-form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent the default form submission
+    // add your form processing logic here
+    
+    // redirect the user to the "cron.html" page
+    window.location.href = 'cron.html';
+  });
+  
+
+}
+
+handleFormSubmit();
+setFormData();
